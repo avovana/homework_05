caeda83849437cbea7d8c579021432451602d478
Vladimir <vladimir.englishbox@gmail.com>
2018-05-23 13:01:34 +0400

    Refactored indents

--- include/graph_editor.h
+++ include/graph_editor.h
@@ -8,36 +8,36 @@
 
 class GraphEditor
 {
-    public:
+public:
     explicit GraphEditor (const std::string & name_) : projectName{name_}
-	{ }
-	
+    { }
+
     void importFromFile(std::string filePath)
-	{ 
-		canvas.importFromFile(filePath);
-	}
+    {
+        canvas.importFromFile(filePath);
+    }
 
     void exportToFile(std::string filePath)
-	{
-		canvas.exportToFile(filePath);
-	}
-	
+    {
+        canvas.exportToFile(filePath);
+    }
+
     void drawLine(Coord from, Coord to)
-	{
-		canvas.drawLine(from, to); 
-	}
-	
+    {
+        canvas.drawLine(from, to);
+    }
+
     void drawCircle(Coord radius)
-	{
-		canvas.drawCircle(radius);
-	}
-	
+    {
+        canvas.drawCircle(radius);
+    }
+
     void deleteLast()
-	{
-		canvas.deleteLast();
-	}
+    {
+        canvas.deleteLast();
+    }
     
-    private:
+private:
     std::string projectName{""};
     Canvas canvas;
     
